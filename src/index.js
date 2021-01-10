a9bcaec087aa6ebb68cdb75cd83216388ec0c9b0
dimabytes <dimabytes@gmail.com>
2021-01-10 21:25:29 +0300

    refactor diff

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-use-before-define */
 import _ from 'lodash';
 import parse from './parsers.js';
 
@@ -24,84 +23,94 @@ const getSign = (type) => {
   }
 };
 
-const formatToStylish = (obj, level) => {
-  const innerData = obj.map((val) => {
+const formatObj = (obj, level) => {
+  const innerData = Object.keys(obj).map((key) => {
+    const val = obj[key];
     const levelIndent = getLevelIndent(level);
-    const sign = getSign(val.type);
-    const value = val.children.length > 0 ? formatToStylish(val.children, level + 1) : val.value;
-    return `${levelIndent}${sign}${val.key}: ${value}`;
+    const outputString = _.isObject(val) ? formatObj(val, level + 1) : val;
+    return `${levelIndent}  ${key}: ${outputString}`;
   }).join('\n');
   return `{\n${innerData}\n${getCloseLevelIndent(level)}}`;
 };
 
-const makeKeyDiff = (obj1, obj2, key) => {
-  const value1 = obj1[key];
-  const value2 = obj2[key];
-
-  if (_.isObject(value1) && _.isObject(value2)) {
-    const sortedObjectsKeys = getSortedObjectsKeys(value1, value2);
-    return {
-      key,
-      value: null,
-      children: makeDiffOnKeys(sortedObjectsKeys, value1, value2),
-      type: CHANGE_TYPES.unchanged,
-    };
-  }
-
-  if (_.has(obj1, key) && _.has(obj2, key)) {
-    return makeTwoValuesDiff(value1, value2, key);
-  }
-  if (_.has(obj1, key)) {
-    return makeOneValueDiff(value1, key, CHANGE_TYPES.removed);
-  }
-  return makeOneValueDiff(value2, key, CHANGE_TYPES.added);
+const formatToStylish = (obj, level) => {
+  const getOutputString = (val) => {
+    if (val.children.length > 0) {
+      return formatToStylish(val.children, level + 1);
+    }
+    if (_.isObject(val.value)) {
+      return formatObj(val.value, level + 1);
+    }
+    return val.value;
+  };
+  const innerData = obj.map((val) => {
+    const levelIndent = getLevelIndent(level);
+    const sign = getSign(val.type);
+    const outputString = getOutputString(val);
+    return `${levelIndent}${sign}${val.key}: ${outputString}`;
+  }).join('\n');
+  return `{\n${innerData}\n${getCloseLevelIndent(level)}}`;
 };
 
 const getSortedObjectsKeys = (obj1, obj2) => _
   .uniq([...Object.keys(obj1), ...Object.keys(obj2)])
   .sort();
 
-const makeDiffOnKeys = (keys, obj1, obj2) => keys
-  .map((newKey) => makeKeyDiff(obj1, obj2, newKey))
-  .flat();
-
-const makeOneValueDiff = (value, key, type) => {
-  if (_.isObject(value)) {
+const makeKeyDiff = (obj1, obj2, key) => {
+  const value1 = obj1[key];
+  const value2 = obj2[key];
+  if (_.isObject(value1) && _.isObject(value2)) {
     return {
       key,
-      children: makeDiffOnKeys(Object.keys(value), value, value),
+      type: CHANGE_TYPES.unchanged,
       value: null,
-      type,
+      children: getSortedObjectsKeys(value1, value2)
+        .flatMap((newKey) => makeKeyDiff(value1, value2, newKey)),
+    };
+  }
+  if (_.has(obj1, key) && _.has(obj2, key)) {
+    if (_.isEqual(value1, value2)) {
+      return {
+        key,
+        type: CHANGE_TYPES.unchanged,
+        value: value1,
+        children: [],
+      };
+    }
+    return [{
+      key,
+      type: CHANGE_TYPES.removed,
+      value: value1,
+      children: [],
+    },
+    {
+      key,
+      type: CHANGE_TYPES.added,
+      value: value2,
+      children: [],
+    }];
+  }
+  if (_.has(obj1, key)) {
+    return {
+      key,
+      type: CHANGE_TYPES.removed,
+      value: value1,
+      children: [],
     };
   }
   return {
     key,
+    type: CHANGE_TYPES.added,
+    value: value2,
     children: [],
-    value,
-    type,
   };
-};
-
-const makeTwoValuesDiff = (value1, value2, key) => {
-  if (_.isEqual(value1, value2)) {
-    return {
-      key,
-      children: [],
-      value: value1,
-      type: CHANGE_TYPES.unchanged,
-    };
-  }
-  return [
-    makeOneValueDiff(value1, key, CHANGE_TYPES.removed),
-    makeOneValueDiff(value2, key, CHANGE_TYPES.added),
-  ];
 };
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const obj1 = parse(filepath1);
   const obj2 = parse(filepath2);
-  const sortedObjectsKeys = getSortedObjectsKeys(obj1, obj2);
-  const diff = makeDiffOnKeys(sortedObjectsKeys, obj1, obj2);
+  const diff = getSortedObjectsKeys(obj1, obj2)
+    .flatMap((newKey) => makeKeyDiff(obj1, obj2, newKey));
   if (format === 'stylish') {
     return formatToStylish(diff, 1);
   }
