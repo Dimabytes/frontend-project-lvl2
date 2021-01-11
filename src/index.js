8f50183379aab875a588e7d2372cd2e7b2f244f2
dimabytes <dimabytes@gmail.com>
2021-01-11 17:33:40 +0300

    fix sort

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import _ from 'lodash';
 import parse from './parsers.js';
 import getFormatter from './formatters/index.js';
 
-const getUniqSortedKeys = (obj1, obj2) => _
-  .uniq([...Object.keys(obj1), ...Object.keys(obj2)])
-  .sort();
+const getUniqSortedKeys = (obj1, obj2) => _([...Object.keys(obj1), ...Object.keys(obj2)])
+  .uniq().sort().value();
 
 const makeKeyDiff = (obj1, obj2, key) => {
   const oldValue = obj1[key];
